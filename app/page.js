d5ee9036c845164bf6512aad8f511e03aba9231d
aquah9210 <f.xavier9210@gmail.com>
2026-05-27 15:54:38 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,10 @@
-import GoshenApp from './GoshenApp'
-export default function Page() {
-  return <GoshenApp />
+'use client';
+
+import { NextStudio } from 'next-sanity/studio';
+import config from '../../../sanity.config';
+
+export const dynamic = 'force-dynamic';
+
+export default function StudioPage() {
+  return <NextStudio config={config} />;
 }
